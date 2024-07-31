5e3ebea0535de4ba7624d3a6a234429eac585ffa
Jan Galek <admin@gcore.cz>
2024-07-31 13:42:06 +0200

    fixup! WIP

--- src/QueryObject/Movie/MovieQueryObject.php
+++ src/QueryObject/Movie/MovieQueryObject.php
@@ -6,12 +6,26 @@ namespace SledovaniTV\QueryObject\Movie;
 
 use SledovaniTV\Lib\Database\Queryable;
 use SledovaniTV\Lib\Database\QueryObject;
+use SledovaniTV\Model\Movie\Movie;
 
 class MovieQueryObject extends QueryObject
 {
 
 	protected function doCreateQuery(Queryable $repository): \Doctrine\ORM\Query|\Doctrine\ORM\QueryBuilder
 	{
-		return $repository->createQueryBuilder('m');
+		return $repository->createQueryBuilder()
+			->select('movie')->from(Movie::class, 'movie')
+			->innerJoin('movie.author', 'author')->addSelect('author');
+	}
+
+	public function postFetch(Queryable $repository, \Iterator $iterator): void
+	{
+		$ids = array_keys(iterator_to_array($iterator, true));
+
+		$repository->createQueryBuilder()
+			->select('partial movie.{ids}')->from(Movie::class, 'movie')
+			->leftJoin('movie.genres', 'genres')->addSelect('genres')
+			->andWhere('movie.id IN (:ids)')->setParameter('ids', $ids)
+			->getQuery()->getResult();
 	}
 }
--- src/Repository/Movie/MovieRepository.php
+++ src/Repository/Movie/MovieRepository.php
@@ -5,6 +5,7 @@ namespace SledovaniTV\Repository\Movie;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use SledovaniTV\Model\Movie\Movie;
+use SledovaniTV\QueryObject\Movie\MovieQueryObject;
 
 class MovieRepository extends EntityRepository
 {
@@ -12,5 +13,11 @@ class MovieRepository extends EntityRepository
 	public function __construct(ManagerRegistry $registry)
 	{
 		parent::__construct($registry, Movie::class);
+	}
+
+	public function getList()
+	{
+		$qb = new MovieQueryObject();
+
 	}
 }
